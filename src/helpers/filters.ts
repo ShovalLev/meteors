706ba7319e895e696c0fb035b0e5c18bf33f3a58
Shoval Lev <61658153+ShovalLev@users.noreply.github.com>
2023-02-19 18:42:13 +0200

    Update filters.ts

--- src/helpers/filters.ts
+++ src/helpers/filters.ts
@@ -1,15 +1,15 @@
 import { Meteor } from '../types';
 
 const checkIfYearsAreEqual = (d1: Date, d2: Date) => d1.getFullYear() === d2.getFullYear();
-const checkIfMeteorMassBiggerThanFilteredMASS = 
-    (meteorMass: string, filterMass: string) => parseInt(meteorMass) >= parseInt(filterMass);
+const checkIfMeteorMassLargerThanFilteredMASS = 
+    (meteorMass: string, filterMass: string) => parseInt(meteorMass) > parseInt(filterMass);
 
 export const getMostRelevantYearForMassFilter = (data: Array<Meteor>, massFilter?: string): Date | undefined => {
     if (!massFilter) {
         return;
     }
 
-    const relevantMeteor = data.find((meteor) => checkIfMeteorMassBiggerThanFilteredMASS(meteor.mass, massFilter));
+    const relevantMeteor = data.find((meteor) => checkIfMeteorMassLargerThanFilteredMASS(meteor.mass, massFilter));
 
     if (relevantMeteor) {
         return new Date(relevantMeteor.year);
@@ -24,7 +24,7 @@ export const filterData = (data: Array<Meteor>, pickedDate?: Date, massFilter?: 
     const filteredData = data.filter((meteor) => {
         if (massFilter) {
             return checkIfYearsAreEqual(new Date(meteor.year), pickedDate) 
-                && checkIfMeteorMassBiggerThanFilteredMASS(meteor.mass, massFilter);
+                && checkIfMeteorMassLargerThanFilteredMASS(meteor.mass, massFilter);
         }
         return checkIfYearsAreEqual(new Date(meteor.year), pickedDate);
     });
